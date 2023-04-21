3249d28bb75d11ff3268df35b0db1e104c701302
christianjensen2903 <cmj290300@gmail.com>
2023-04-21 11:56:27 +0200

    🧹 Remove some unnessary code

--- linkedin/settings.py
+++ linkedin/settings.py
@@ -15,16 +15,5 @@ BOT_NAME = "linkedin"
 SPIDER_MODULES = ["linkedin.spiders"]
 NEWSPIDER_MODULE = "linkedin.spiders"
 
-# HTTPCACHE_ENABLED = True
-
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
-
-DOWNLOADER_MIDDLEWARES = {
-    "scrapeops_scrapy_proxy_sdk.scrapeops_scrapy_proxy_sdk.ScrapeOpsScrapyProxySdk": 725,
-}
-REQUEST_FINGERPRINTER_CLASS = "scrapy_zyte_api.ScrapyZyteAPIRequestFingerprinter"
-
-SCRAPEOPS_API_KEY = os.getenv("SCRAPEOPS_API_KEY")
-SCRAPEOPS_PROXY_ENABLED = True
-SCRAPEOPS_PROXY_SETTINGS = {"country": "us"}
